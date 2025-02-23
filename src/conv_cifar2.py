08717591c4fc643b6c0d15bdcd3b374e6ad42679
Welby Seely <welbyseely@gmail.com>
2025-02-22 20:36:54 -0500

    base cifar cnn

--- src/conv_cifar2.py
+++ src/conv_cifar2.py
@@ -1,17 +1,16 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from keras import layers, models
 import matplotlib.pyplot as plt
+from tf_keras import datasets
 
 with tf.device('/GPU:0'):
-    mnist = tf.keras.datasets.cifar10
-    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    cifar10 = datasets.cifar10
+    (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
     train_images = train_images.astype('float32') / 255.0
     test_images = test_images.astype('float32') / 255.0
-    train_images = train_images.reshape((train_images.shape[0], 28, 28, 1))
-    test_images = test_images.reshape((test_images.shape[0], 28, 28, 1))
-
+    
     model = models.Sequential([
-        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
         layers.MaxPooling2D((2, 2)),
         layers.Conv2D(64, (3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)),
@@ -24,8 +23,20 @@ with tf.device('/GPU:0'):
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     history = model.fit(train_images, train_labels,
-                        epochs=5,
+                        epochs=50,
                         batch_size=64,
                         validation_split=0.1)
+    train_accuracy, val_accuracy = history.history['accuracy'], history.history['val_accuracy']
+
+    epochs = range(1, len(train_accuracy) + 1)
     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
     print(f'\nTest accuracy: {test_acc:.4f}')
+    
+    plt.plot(epochs, train_accuracy, 'g', label='Training Accuracy')
+    plt.plot(epochs, val_accuracy, 'b', label='Validation Accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
